f78516e75169e92817da851b056415e554dd41e1
Max Wittal <automyinc@gmail.com>
2018-08-25 23:18:20 +0200

    renamed vnxfilesystem

--- src/vnxfilesystem.cpp
+++ src/vnxfilesystem.cpp
@@ -16,27 +16,17 @@ int main(int argc, char** argv) {
 	options["domain"] = "server domain topic";
 	options["input"] = "request input topic";
 	
-	vnx::init("vnxfilesystemserver", argc, argv, options);
+	vnx::init("vnxfilesystem", argc, argv, options);
 	
-	/*
-	 * By default we create a local UNIX node for this example.
-	 */
-	std::string node = "vnxfilesystemserver.sock";
+	std::string node = "vnxfilesystem.sock";
 	vnx::read_config("node", node);
 	
 	{
-		/*
-		 * First we start a terminal to display log messages for us.
-		 */
 		vnx::Handle<vnx::Terminal> terminal = new vnx::Terminal("Terminal");
 		terminal.start_detached();
 	}
 	
 	{
-		/*
-		 * The Server will listen on and accept client connections to this process,
-		 * depending on the type of Endpoint either on a TCP port or on a UNIX domain socket.
-		 */
 		vnx::Handle<vnx::Server> server = new vnx::Server("Server", vnx::Endpoint::from_url(node));
 		server.start_detached();
 	}
@@ -55,18 +45,6 @@ int main(int argc, char** argv) {
 		module->input = "internal.filesystem.request";
 		module.start_detached();
 	}
-	
-//	vnx::Publisher publisher;
-//	while(vnx::do_run()) {
-//		std::shared_ptr<vnx::web::Request> request = vnx::web::Request::create();
-//		request->id = vnx::Hash128::rand();
-//		request->id = request_type_e::READ;
-//		request->path = "/LICENSE";
-//		request->channel = "test.channel";
-//		request->time_stamp_ms = vnx::get_time_millis();
-//		publisher.publish(request, "internal.filesystem.request");
-//		::usleep(1);
-//	}
 	
 	/*
 	 * Wait until shutdown.
