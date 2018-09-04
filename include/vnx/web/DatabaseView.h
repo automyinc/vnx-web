1b9410a6a779f2e3c5b05b0b4e2b90e1bd6f3727
Max Wittal <automyinc@gmail.com>
2018-09-04 10:03:20 +0200

    added login + session to DatabaseView

--- include/vnx/web/DatabaseView.h
+++ include/vnx/web/DatabaseView.h
@@ -21,17 +21,34 @@ protected:
 	
 	void update();
 	
-private:
-	void render_overview(std::ostream& out);
+	void maintain();
 	
-	void render_table_index(std::ostream& out, const std::string& table, const Object& parameter);
+private:
+	std::shared_ptr<Content> render(std::shared_ptr<const Request> request,
+									std::shared_ptr<const Object> parameter) const;
+	
+	std::shared_ptr<Content> login(std::shared_ptr<const Request> request,
+									std::shared_ptr<const Object> parameter) const;
+	
+	void render_overview(std::ostream& out) const;
+	
+	void render_table_index(std::ostream& out, const std::string& table, const Object& parameter) const;
 	
 	void render_table(std::ostream& out, const std::vector<Object>& result,
-				const std::vector<std::string>& fields, const std::map<std::string, std::string>& field_header);
+				const std::vector<std::string>& fields, const std::map<std::string, std::string>& field_header) const;
 	
 private:
 	std::shared_ptr<Provider> provider;
 	std::shared_ptr<DatabaseClient> client;
+	
+	struct session_t {
+		bool is_valid = false;
+		int64_t create_time = 0;
+	};
+	
+	std::string ssid_key;
+	std::unordered_map<std::string, session_t> sessions;
+	session_t* current = 0;
 	
 };
 
--- src/DatabaseView.cpp
+++ src/DatabaseView.cpp
@@ -3,6 +3,7 @@
 #include <vnx/web/Response.hxx>
 #include <vnx/web/File.hxx>
 #include <vnx/web/Parameter.hxx>
+#include <vnx/web/SetCookie.hxx>
 #include <vnx/query/package.h>
 
 #include <sstream>
@@ -18,6 +19,11 @@ DatabaseView::DatabaseView(const std::string& _vnx_name)
 
 void DatabaseView::main() {
 	
+	if(password.size() < 6) {
+		log(ERROR).out << "Password too short!";
+		return;
+	}
+	
 	{
 		auto provider = Provider::create();
 		provider->id = Hash128::rand();
@@ -28,14 +34,17 @@ void DatabaseView::main() {
 	
 	client = std::make_shared<DatabaseClient>(database);
 	
+	ssid_key = "vnx-4fbe2728-" + database;
+	
 	subscribe(input);
 	
 	set_timer_millis(500, std::bind(&DatabaseView::update, this));
+	set_timer_millis(5000, std::bind(&DatabaseView::maintain, this));
 	
 	Super::main();
 }
 
-void DatabaseView::render_overview(std::ostream& out) {
+void DatabaseView::render_overview(std::ostream& out) const {
 	auto info = client->get_table_info();
 	out << "<table>\n";
 	out << "<tr><th>Table</th><th>Rows</th></tr>\n";
@@ -51,7 +60,7 @@ void DatabaseView::render_overview(std::ostream& out) {
 void DatabaseView::render_table(	std::ostream& out,
 									const std::vector<Object>& result,
 									const std::vector<std::string>& fields,
-									const std::map<std::string, std::string>& field_header)
+									const std::map<std::string, std::string>& field_header) const
 {
 	out << "<table>\n";
 	out << "<tr>\n";
@@ -75,7 +84,7 @@ void DatabaseView::render_table(	std::ostream& out,
 	out << "</table>\n";
 }
 
-void DatabaseView::render_table_index(std::ostream& out, const std::string& table, const Object& parameter) {
+void DatabaseView::render_table_index(std::ostream& out, const std::string& table, const Object& parameter) const {
 	
 	auto query = query::select(table, 0, 0, query::limit(1000));
 	
@@ -97,6 +106,10 @@ void DatabaseView::render_table_index(std::ostream& out, const std::string& tabl
 				query.aggregates[field] = query::max(query::field(field));
 				field_header[field] = "MAX(" + field + ")";
 			}
+			if(func == "AVG") {
+				query.aggregates[field] = query::avg(query::field(field));
+				field_header[field] = "AVG(" + field + ")";
+			}
 		}
 	}
 	{
@@ -105,7 +118,6 @@ void DatabaseView::render_table_index(std::ostream& out, const std::string& tabl
 			query.group_by = query::group_by(field);
 			query.aggregates["_N_"] = query::count();
 			field_header[field] = "GROUP(" + field + ")";
-			field_header["_N_"] = "COUNT()";
 		}
 	}
 	{
@@ -140,6 +152,9 @@ void DatabaseView::render_table_index(std::ostream& out, const std::string& tabl
 	
 	out << "<a href=\"..\">Overview</a>\n";
 	out << "<a href=\"?\">Reset</a>\n";
+	out << "<div class=\"right\">\n";
+	out << "<a href=\"" << path.to_string() << "logout\">Logout</a>";
+	out << "</div>\n";
 	out << "<hr>\n";
 	out << "<form method=\"get\">\n";
 	for(const auto& col : fields) {
@@ -148,6 +163,7 @@ void DatabaseView::render_table_index(std::ostream& out, const std::string& tabl
 		out << "<option value=\"SUM\">SUM(" << col << ")</option>\n";
 		out << "<option value=\"MIN\">MIN(" << col << ")</option>\n";
 		out << "<option value=\"MAX\">MAX(" << col << ")</option>\n";
+		out << "<option value=\"AVG\">AVG(" << col << ")</option>\n";
 		out << "</select>\n";
 	}
 	out << " GROUP BY <select name=\"group_by\">\n";
@@ -177,13 +193,9 @@ void DatabaseView::render_table_index(std::ostream& out, const std::string& tabl
 	render_table(out, result, fields, field_header);
 }
 
-void DatabaseView::handle(std::shared_ptr<const ::vnx::web::Request> request) {
-	
-	auto parameter = std::dynamic_pointer_cast<const Parameter>(request->parameter);
-	
-	auto response = Response::create();
-	response->is_for_request(request);
-	
+std::shared_ptr<Content> DatabaseView::render(	std::shared_ptr<const Request> request,
+												std::shared_ptr<const Object> parameter) const
+{
 	std::ostringstream out;
 	out << "<!DOCTYPE html>\n";
 	out << "<html>\n<head>\n";
@@ -193,6 +205,7 @@ void DatabaseView::handle(std::shared_ptr<const ::vnx::web::Request> request) {
 	out << "th, td {border: 1px solid black;}\n";
 	out << ".tr20 {background: #eeeeee;}\n";
 	out << ".tr21 {background: #ffffff;}\n";
+	out << ".right {text-align: right; float: right;}\n";
 	out << "</style>\n";
 	out << "</head>\n<body>\n";
 	
@@ -202,8 +215,12 @@ void DatabaseView::handle(std::shared_ptr<const ::vnx::web::Request> request) {
 		} else if(request->path.size() >= 2) {
 			if(request->path[1] == "table") {
 				if(request->path.size() >= 3) {
-					render_table_index(out, request->path[2], parameter ? parameter->field : Object());
+					render_table_index(out, request->path[2], parameter ? *parameter : Object());
 				}
+			}
+			if(request->path[1] == "logout") {
+				current->is_valid = false;
+				out << "<h2>Logged out.</h2>\n";
 			}
 		}
 	} catch(const std::exception& ex) {
@@ -216,13 +233,96 @@ void DatabaseView::handle(std::shared_ptr<const ::vnx::web::Request> request) {
 	file->mime_type = "text/html";
 	file->time_stamp_ms = vnx::get_time_millis();
 	file->data = out.str();
-	response->result = file;
+	return file;
+}
+
+std::shared_ptr<Content> DatabaseView::login(	std::shared_ptr<const Request> request,
+												std::shared_ptr<const Object> parameter) const
+{
+	std::ostringstream out;
+	out << "<!DOCTYPE html>\n";
+	out << "<html>\n<head>\n";
+	out << "<title>Login</title>\n";
+	out << "<style>\n";
+	out << "</style>\n";
+	out << "</head>\n<body>\n";
+	
+	out << "<h2>Login to '" << database << "'</h2>\n";
+	
+	out << "<form method=\"post\">\n";
+	out << "Password:<input type=\"password\" name=\"password\">\n";
+	out << "<input type=\"submit\">\n";
+	out << "</form>\n";
+	
+	if(parameter && parameter->field.count("password")) {
+		out << "<br>Error: Wrong password!<br>\n";
+	}
+	
+	out << "</body>\n</html>\n";
+	
+	auto file = File::create();
+	file->mime_type = "text/html";
+	file->time_stamp_ms = vnx::get_time_millis();
+	file->data = out.str();
+	return file;
+}
+
+void DatabaseView::handle(std::shared_ptr<const ::vnx::web::Request> request) {
+	
+	auto response = Response::create();
+	response->is_for_request(request);
+	
+	current = 0;
+	{
+		auto iter = request->session.find(ssid_key);
+		if(iter != request->session.end()) {
+			auto it = sessions.find(iter->second);
+			if(it != sessions.end()) {
+				current = &it->second;
+			}
+		}
+	}
+	
+	auto parameter = std::dynamic_pointer_cast<const Object>(request->parameter);
+	
+	if(current) {
+		if(current->is_valid) {
+			response->result = render(request, parameter);
+		} else {
+			if(parameter && (*parameter)["password"].to_string_value() == password) {
+				current->is_valid = true;
+				response->result = ErrorCode::create_with_message(ErrorCode::MOVED_TEMPORARILY, path.to_string());
+			} else {
+				response->result = login(request, parameter);
+			}
+		}
+	} else {
+		const std::string ssid = Hash128::rand().to_hex_string();
+		session_t& session = sessions[ssid];
+		session.create_time = vnx::get_time_millis();
+		response->result = SetCookie::create(ssid_key, ssid, path.to_string() + "login");
+	}
 	
 	publish(response, request->get_return_channel());
 }
 
 void DatabaseView::update() {
 	publish(provider, domain);
+}
+
+void DatabaseView::maintain() {
+	std::vector<std::string> list;
+	const int64_t now = vnx::get_time_millis();
+	for(const auto& entry : sessions) {
+		if(!entry.second.is_valid
+			&& now - entry.second.create_time > 60*1000)
+		{
+			list.push_back(entry.first);
+		}
+	}
+	for(const auto& ssid : list) {
+		sessions.erase(ssid);
+	}
 }
 
 
